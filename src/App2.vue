a70560208e928b92b05c4d9c100f240d9c2a14e5
charlie <daad777@126.com>
2018-01-23 19:36:49 +0800

    add style test

--- src/App2.vue
+++ src/App2.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
+  <div id="test">{{text}}
     hah23345
   </div>
 </template>
 <script>
-
+export default {
+  data(){
+    return{
+      text: 'ab'
+    }
+  }
+}
 
 </script>
+<style>
+#test{
+  color:red;
+}
+
+</style>
